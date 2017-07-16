c2c0c391d29b17dbc54852aa8e7d622bd644b745
Mark White <markhwhiteii@gmail.com>
2017-07-15 20:05:37 -0500

    updated documentation

--- R/model4.R
+++ R/model4.R
@@ -1,17 +1,16 @@
 #' Model 4 from the PROCESS Macro: Simple Mediation
 #' 
 #' This function will perform simple mediation, using the `lavaan` package.
-#' It uses maximum likelihood estimation and performs bias-corrected bootstrap 
-#' resampling for the confidence intervals.
+#' It uses bias-corrected bootstrap resampling for the confidence intervals.
 #' 
-#' @param iv The name of the independent variable, as a character string
-#' @param dv The name of the dependent variable, as a character string
-#' @param med The name of the mediator, as a character string
-#' @param data The data frame with the relevant variables
-#' @param samples The number of bootstrap resamples. Defaults to 5000
+#' @param iv The name of the independent variable, as a character string.
+#' @param dv The name of the dependent variable, as a character string.
+#' @param med The name of the mediator, as a character string.
+#' @param data The data frame with the relevant variables.
+#' @param samples The number of bootstrap resamples. Defaults to 5000.
 #' @return Coefficients, standard errors, z-values, p-values, and confidence intervals
 #' for all estimated parameters. The indirect effect will not return standard errors,
-#' z-values, or p-values
+#' z-values, or p-values.
 #' @export
 model4 <- function(iv, dv, med, data, samples=5000) {
   model <- paste0(med, " ~ a*", iv, "
