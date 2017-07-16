dd623e0cc0f5d1949faaf6db8726064de1a0107a
Mark White <markhwhiteii@gmail.com>
2017-07-15 20:04:00 -0500

    updated documentation

--- R/model1.R
+++ R/model1.R
@@ -3,15 +3,15 @@
 #' Performs a simple two-way interaction and returns simple slopes, as per
 #' Model 1 from the PROCESS Macro.
 #' 
-#' @param iv The name of the independent variable, as a character string
-#' @param dv The name of the dependent variable, as a character string
+#' @param iv The name of the independent variable, as a character string.
+#' @param dv The name of the dependent variable, as a character string.
 #' @param mod The name of the moderator, as a character string. If the moderator
 #' is numeric with only 0s and 1s, it will return the simple slopes at the values of
 #' 0 and 1; if it is numeric otherwise, it will return the simple slopes at a standard
-#' deviation below the mean, a standard deviation above the mean, and at the mean
-#' @param data The data frame with the relevant variables
+#' deviation below the mean, a standard deviation above the mean, and at the mean.
+#' @param data The data frame with the relevant variables.
 #' @return The coefficients, standard errors, t-statistics, and p-values of 
-#' the regression coefficients table and the simple slopes
+#' the regression coefficients table and the simple slopes.
 #' @export
 model1 <- function(iv, dv, mod, data) {
   coefs <- broom::tidy(lm(data[,dv] ~ data[,iv]*data[,mod]))
