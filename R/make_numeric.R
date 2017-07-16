a9329087e30c6ae419b017e77db9d71e6c817d33
Mark White <markhwhiteii@gmail.com>
2017-07-15 19:51:24 -0500

    update documentation

--- R/make_numeric.R
+++ R/make_numeric.R
@@ -5,12 +5,12 @@
 #' it a vector of 0s and 1s, where the reference value is coded zero. It returns a
 #' data frame with this new variable added onto it.
 #' 
-#' @param var The name of the dichotomous factor variable to be converted, as a character string
+#' @param var The name of the dichotomous factor variable to be converted, as a character string.
 #' @param ref The value of the factor that you consider the reference category, as a character string; 
-#' this will be converted to zero
-#' @param newvar The name of the new variable, as a character string
-#' @param data The data frame
-#' @return A data frame with the new variable added onto it
+#' this will be converted to zero.
+#' @param newvar The name of the new variable, as a character string.
+#' @param data The data frame.
+#' @return A data frame with the new variable added onto it.
 #' @export
 make_numeric <- function(var, ref, newvar, data) {
   data[,newvar] <- ifelse(data[,var]==ref, 0, 1)
